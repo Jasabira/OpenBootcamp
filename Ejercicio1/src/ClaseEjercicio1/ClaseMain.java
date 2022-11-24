80474b64ed3b0db0bf8a295476b42ca2892852cc
jasabira <jasbircoro.dev@gmail.com>
2022-11-24 13:43:18 -0300

    first commit

---
+++ Ejercicio1/src/ClaseEjercicio1/ClaseMain.java
@@ -0,0 +1,8 @@
+package ClaseEjercicio1;
+
+public class ClaseMain {
+    public static void main(String[] args) {
+        //Los tipos de datos más comunes son int, long, double, boolean, String.
+        
+    }
+}
