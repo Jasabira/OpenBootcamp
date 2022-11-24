0478af4368dbea158cae55d7d902ee79c94a86b8
jasabira <jasbircoro.dev@gmail.com>
2022-11-24 13:46:24 -0300

    comprobando funcionamiento

--- Ejercicio1/src/ClaseEjercicio1/ClaseMain.java
+++ Ejercicio1/src/ClaseEjercicio1/ClaseMain.java
@@ -3,6 +3,6 @@ package ClaseEjercicio1;
 public class ClaseMain {
     public static void main(String[] args) {
         //Los tipos de datos más comunes son int, long, double, boolean, String.
-        
+        //Tipos primitivos: int, short, long, double, float, boolean, byte, char.
     }
 }
