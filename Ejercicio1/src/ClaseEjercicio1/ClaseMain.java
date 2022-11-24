0379c48d7a837c34406ede262d5cf33b85f80163
jasabira <jasbircoro.dev@gmail.com>
2022-11-24 13:55:16 -0300

    Ejercicio completo

--- Ejercicio1/src/ClaseEjercicio1/ClaseMain.java
+++ Ejercicio1/src/ClaseEjercicio1/ClaseMain.java
@@ -4,5 +4,28 @@ public class ClaseMain {
     public static void main(String[] args) {
         //Los tipos de datos más comunes son int, long, double, boolean, String.
         //Tipos primitivos: int, short, long, double, float, boolean, byte, char.
+
+        int numero = 0;
+        short numero2 = 345;
+        long numeroLargo = 345346367;
+        double numeroDecimal = 3.14;
+        float numero3 = 234;
+        byte numero4 = 5;
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        char caracter = 'a';
+        String nombre = "jasbir";
+
+        System.out.println("El int es: " + numero);
+        System.out.println("El short es: " + numero2);
+        System.out.println("El long es: " + numeroLargo);
+        System.out.println("El double es: " + numeroDecimal);
+        System.out.println("El float es: " + numero3);
+        System.out.println("El byte es: " + numero4);
+        System.out.println("El boolean es: " + verdadero + " y " + falso);
+        System.out.println("El char es: " + caracter);
+        System.out.println("El String es: " + nombre);
     }
 }
